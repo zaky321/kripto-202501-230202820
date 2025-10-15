ff52ba3f3246f850f3ff541eb6ed7ede911999e9
zaky321 <141202616+zaky321@users.noreply.github.com>
2025-10-15 21:02:17 +0700

    Update and rename code-example.py to simple_crypto.py

---
+++ praktikum/week2-cryptosystem/src/simple_crypto.py
@@ -0,0 +1,33 @@
+
+# file: praktikum/week2-cryptosystem/src/simple_crypto.py
+
+def encrypt(plaintext, key):
+    result = ""
+    for char in plaintext:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift + key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+def decrypt(ciphertext, key):
+    result = ""
+    for char in ciphertext:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift - key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+if __name__ == "__main__":
+    message = "<sofyan muzaki>"
+    key = 5
+
+    enc = encrypt(message, key)
+    dec = decrypt(enc, key)
+
+    print("Plaintext :", message)
+    print("Ciphertext:", enc)
+    print("Decrypted :", dec)
