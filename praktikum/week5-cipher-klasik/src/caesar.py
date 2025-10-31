79cd28e27832b5095615c4ff7e52420fbb40e369
zaky321 <141202616+zaky321@users.noreply.github.com>
2025-10-31 23:09:42 +0700

    Create caesar.py

---
+++ praktikum/week5-cipher-klasik/src/caesar.py
@@ -0,0 +1,21 @@
+def caesar_encrypt(plaintext, key):
+    result = ""
+    for char in plaintext:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift + key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(ciphertext, key):
+    return caesar_encrypt(ciphertext, -key)
+
+# Contoh uji
+msg = "SOFYAN NIH BOS"
+key = 3
+enc = caesar_encrypt(msg, key)
+dec = caesar_decrypt(enc, key)
+print("Plaintext :", msg)
+print("Ciphertext:", enc)
+print("Decrypted :", dec)
