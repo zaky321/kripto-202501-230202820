c5c5978c3ee435c8d9973b4c72385161347b8532
zaky321 <141202616+zaky321@users.noreply.github.com>
2025-10-31 23:09:17 +0700

    Create vigenere.py

---
+++ praktikum/week5-cipher-klasik/src/vigenere.py
@@ -0,0 +1,36 @@
+def vigenere_encrypt(plaintext, key):
+    result = []
+    key = key.lower()
+    key_index = 0
+    for char in plaintext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - 97
+            base = 65 if char.isupper() else 97
+            result.append(chr((ord(char) - base + shift) % 26 + base))
+            key_index += 1
+        else:
+            result.append(char)
+    return "".join(result)
+
+def vigenere_decrypt(ciphertext, key):
+    result = []
+    key = key.lower()
+    key_index = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - 97
+            base = 65 if char.isupper() else 97
+            result.append(chr((ord(char) - base - shift) % 26 + base))
+            key_index += 1
+        else:
+            result.append(char)
+    return "".join(result)
+
+# Contoh uji
+msg = "SOFYAN NICHH"
+key = "KEY"
+enc = vigenere_encrypt(msg, key)
+dec = vigenere_decrypt(enc, key)
+print("Plaintext :", msg)
+print("Ciphertext:", enc)
+print("Decrypted :", dec)
