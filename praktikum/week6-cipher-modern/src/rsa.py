46d057ad9762d878176ea2e9ef2d7a79a994f833
zaky321 <141202616+zaky321@users.noreply.github.com>
2025-11-13 22:31:38 +0700

    Create rsa.py

---
+++ praktikum/week6-cipher-modern/src/rsa.py
@@ -0,0 +1,18 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+# Generate key pair
+key = RSA.generate(2048)
+private_key = key
+public_key = key.publickey()
+
+# Enkripsi dengan public key
+cipher_rsa = PKCS1_OAEP.new(public_key)
+plaintext = b"RSA Example"
+ciphertext = cipher_rsa.encrypt(plaintext)
+print("Ciphertext:", ciphertext)
+
+# Dekripsi dengan private key
+decipher_rsa = PKCS1_OAEP.new(private_key)
+decrypted = decipher_rsa.decrypt(ciphertext)
+print("Decrypted:", decrypted.decode())
