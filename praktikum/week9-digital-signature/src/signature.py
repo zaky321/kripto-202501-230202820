f72590339ab701ce2dd476840db33a3814ca765e
zaky321 <sofyan.muzaqi@gmail.com>
2025-12-27 22:34:59 +0700

    Save local signature implementation

---
+++ praktikum/week9-digital-signature/src/signature.py
@@ -0,0 +1,32 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+
+# Generate pasangan kunci RSA
+key = RSA.generate(2048)
+private_key = key
+public_key = key.publickey()
+
+# Pesan yang akan ditandatangani
+message = b"Hello, ini pesan penting."
+h = SHA256.new(message)
+
+# Buat tanda tangan dengan private key
+signature = pkcs1_15.new(private_key).sign(h)
+print("Signature:", signature.hex())
+
+try:
+    pkcs1_15.new(public_key).verify(h, signature)
+    print("Verifikasi berhasil: tanda tangan valid.")
+except (ValueError, TypeError):
+    print("Verifikasi gagal: tanda tangan tidak valid.")
+
+    # Modifikasi pesan
+fake_message = b"Hello, ini pesan palsu."
+h_fake = SHA256.new(fake_message)
+
+try:
+    pkcs1_15.new(public_key).verify(h_fake, signature)
+    print("Verifikasi berhasil (seharusnya gagal).")
+except (ValueError, TypeError):
+    print("Verifikasi gagal: tanda tangan tidak cocok dengan pesan.")
