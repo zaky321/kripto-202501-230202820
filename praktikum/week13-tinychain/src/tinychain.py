de0f4885b7d7916397f451221e8733ab33c90369
zaky321 <141202616+zaky321@users.noreply.github.com>
2026-01-14 23:09:49 +0700

    Update and rename code-example.py to tinychain.py

---
+++ praktikum/week13-tinychain/src/tinychain.py
@@ -0,0 +1,45 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, data, timestamp=None):
+        self.index = index
+        self.timestamp = timestamp or time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        value = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)
+        return hashlib.sha256(value.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print(f"Block mined: {self.hash}")
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
+
+    def create_genesis_block(self):
+        return Block(0, "0", "Genesis Block")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+# Uji coba blockchain
+my_chain = Blockchain()
+print("Mining block 1...")
+my_chain.add_block(Block(1, "", "Transaksi A → B: 10 Coin"))
+
+print("Mining block 2...")
+my_chain.add_block(Block(2, "", "Transaksi B → C: 5 Coin"))
